bebbb3eefe4de5eefeaf8a517da0992e3653ddd6
danilomartins <dglmartins@outlook.com>
2017-08-07 00:40:51 -0400

    Create Action Creators

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
+
+export function addRecipe ({ day, recipe, meal }) {
+  return {
+    type: ADD_RECIPE,
+    recipe,
+    day,
+    meal,
+  };
+}
+
+export function removeFromCalendar
