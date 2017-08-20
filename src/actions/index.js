cff0aca800aa38f1ea52a1a29ac0c1e9344171c8
danilomartins <dglmartins@outlook.com>
2017-08-20 09:09:36 -0400

    Shopping List

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-export const ADD_RECIPE = 'ADD_RECIPE';
-export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
+export const ADD_RECIPE = 'ADD_RECIPE'
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
 
 export function addRecipe ({ day, recipe, meal }) {
   return {
@@ -7,13 +7,13 @@ export function addRecipe ({ day, recipe, meal }) {
     recipe,
     day,
     meal,
-  };
+  }
 }
 
-export function removeFromCalendar ({ day, meal, }) {
+export function removeFromCalendar ({ day, meal }) {
   return {
     type: REMOVE_FROM_CALENDAR,
     day,
     meal,
-  };
+  }
 }
